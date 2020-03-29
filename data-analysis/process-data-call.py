c769687f7362abda01adff8f44ee8ce0f0da68a1
pio8909 <40757786+pio8909@users.noreply.github.com>
2020-03-29 17:48:55 +0200

    Update process-data-call.py

--- data-analysis/process-data-call.py
+++ data-analysis/process-data-call.py
@@ -3,7 +3,7 @@ import logging
 import json
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
-username = 'raphaeld'
+username = 'pio8909'
 url = 'https://' + username + '.cartodb.com/api/v2/sql'
 sql = {'q':'SELECT process_data();'}
 logger.info('Pinging {url} with query: {sql}'.format(url=url,sql=sql['q']))
